c96a215fb4bb6b91382c38ccddaec6ec985337c1
hilinaayenew <hilinaayenew47@gmail.com>
2025-05-01 19:12:17 +0300

    NEW

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './index.css'; // Ensure this is correct
-import Navbar from '../components/navbar/Navbar';
-import Ofwhite from '../components/ofwhite/Ofwhite';
-import Services from '../components/services/Services';
-import Benefit from '../components/benefit/Benefit';
-import Plan from '../components/plan/Plan';
-import Work from '../components/work/Work';
-import Work2 from '../components/work2/Work2';
-import Footer from '../components/footer/Footer';
+import Navbar from './components/navbar/Navbar';
+import Ofwhite from './components/ofwhite/Ofwhite';
+import Services from './components/services/Services';
+import Benefit from './components/benefit/Benefit';
+import Plan from './components/plan/Plan';
+import Work from './components/work/Work';
+import Work2 from './components/work2/Work2';
+import Footer from './components/footer/Footer';
 const App = () => {
   return <> 
   <main className='overflow-x-hidden '>
